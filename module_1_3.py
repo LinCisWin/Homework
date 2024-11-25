c10846099220f198150f4febf74c5149f5ebc749
LinCisWin <sashuta.glinskiy@mail.ru>
2024-11-25 09:41:30 +0300

    module_1_3

---
+++ module_1_3.py
@@ -0,0 +1,15 @@
+#Создаем переменную "name" и присваиваем ей значение моего имени.
+name = 'Alexander'
+print('Name:', name)
+
+#Создаем переменную "age" и присваиваем ей значение моего возраста.
+age = 23
+print('Age:', age)
+
+#Создаем переменную "new_age" и присваиваем ей значение нового возраста.
+new_age = age + 1
+print('New Age:', new_age)
+
+#Создаем переменную "is_student" и присваиваем ей значение "Истина".
+is_students = True
+print('Is Student:', is_students)
