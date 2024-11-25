39ef60c1c06f26baf4fdf4bee5a50d860ff00396
LinCisWin <sashuta.glinskiy@mail.ru>
2024-11-25 10:02:13 +0300

    module_1_6

---
+++ module_1_6.py
@@ -0,0 +1,7 @@
+homework = 12
+hour = 1.5
+name_course = 'Python'
+time_for_homework = hour / homework
+
+print('Курс:', name_course + ', всего задач:', str(homework) + ', затрачено часов:', str(hour) + ', среднее время выполнения', time_for_homework, 'часа.')
+#(!)Преобразовал переменные в строку, чтобы на выводе получить "всего задач: 12,".
