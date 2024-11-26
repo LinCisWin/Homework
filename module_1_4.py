ba3e42c8aa8cc375975a667c11b071f71f6ab2ec
LinCisWin <sashuta.glinskiy@mail.ru>
2024-11-26 13:12:48 +0300

    module_1_4

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input("Введите слово: ")
+print("Количество символов: ", len(my_string))
+
+print("Строка в верхнем регистре:", my_string.upper())
+
+print("Строка в нижнем регистре:" ,my_string.lower())
+
+print("Удаление пробелов:", my_string.replace(" ", ""))
+
+print("Первый символ строки:", my_string[0])
+
+print("Последний символ строки:", my_string[-1])
